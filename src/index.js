bec3d0c1214853673678b5a77ca087467ea4204e
AshVosem <ash.vosem@gmail.com>
2020-03-03 22:40:19 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,11 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    return expr.match(/.{10}/g)
+    .map(elem => elem.replace(/11/g, '-').replace(/10/g, '.').replace(/00/g,''))
+    .map(elem => elem != '**********' ? elem.replace(elem, MORSE_TABLE[elem]) : ' ')
+    .join('')
+    
 }
 
 module.exports = {
